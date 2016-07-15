701af44784a56d57283cf05ed7ded00360beb5f2
Dr. Talmor Meir <talmormeir@users.noreply.github.com>
2016-07-15 09:37:17 -0400

    Add files via upload

---
+++ server.R
@@ -0,0 +1,114 @@
+library(data.table)
+library(ggplot2)
+library(shiny)
+library(rsconnect)
+
+#run these lines to have it upload to  a local machine
+#setwd("C:/Users/i57757/Documents/ROI")
+#runApp(appDir ="my_app", port = 7654, host ="172.18.192.22",display="normal")
+
+
+shinyServer(function(input, output) {
+    
+    output$TotalPremium <- renderText({paste("    Total Expected Premiums:    ","   $",prettyNum(input$TotalDrivers*input$PolicyValue,big.mark=","))})#total Premium calculations
+    
+    
+    output$TechnologyCost<-renderText({paste("    Technology Cost:    ","  -$",
+                                             prettyNum(input$Technology*35,big.mark=","))})  #the cost of Dongles
+    
+    output$DefectionRate<-renderText({paste("    Defection Rate:    ",
+                                            floor((((10/100)*(input$TotalDrivers-(input$TotalDrivers*input$Telematics/100)))+
+                                                       ((as.numeric(input$DefectionRate)/100)*(input$TotalDrivers*input$Telematics/100)))),"    Drivers/year")}) #Drivers lost per year
+    
+   
+    output$DefectionSavings<-renderText({paste("    Savings due to telematics' reduced defection rates:    ","  +$",
+                                               prettyNum(round((((10/100)*(input$TotalDrivers))*2*input$PolicyValue)- #2 is because it costs 2 policies to replace 1 policy
+                                                                   ((((10/100)*(input$TotalDrivers-(input$TotalDrivers*input$Telematics/100)))+
+                                                                         ((as.numeric(input$DefectionRate)/100)*(input$TotalDrivers*input$Telematics/100)))*2*input$PolicyValue),digits=2),big.mark=","))}) #$ Savings due to alternative defection rate
+    
+    
+    
+    
+    output$population<-reactivePlot(function(){
+        disscount<-c(20,15,10,5,0)
+        pop<-c(0.0351,0.228,0.251,0.139,0.3469)
+        DT<-cbind(disscount,pop)
+        DT<-as.data.table(DT)
+        for (i in 1:nrow(DT)){
+            DT$popcount[i]<-(DT$pop[i])*((input$Telematics/100)*input$TotalDrivers)}  
+        for (i in 1:nrow(DT)){
+            DT$TotalDiscount[i]<-DT$popcount[i]*((DT$disscount[i]/100)*input$PolicyValue)}
+        
+        output$TotalDiscount<-renderText({
+            paste("    Total discount given:    -$",prettyNum(round(sum(DT$TotalDiscount),digits=1),big.mark=","))})#Total discount given per portfolio
+        
+        p<-ggplot(DT, aes(x = disscount,y=popcount)) + geom_bar(stat="identity",fill="#0066CC",colour="black")+theme_bw()+
+            
+            theme(axis.title = element_text(face="bold", size=14),axis.text= element_text(face="bold", size=14))+ 
+            theme(plot.title = element_text(family = "Ariel", color="black", face="bold", size=14))+
+            scale_fill_brewer(palette="Set1")+
+            ggtitle("Distribution of Discounts")+labs(x="% Discount",  y = "population count")
+        print(p)},width=500)
+    
+    #capture Rate analysis: 
+    t<-reactive({round(
+        (as.numeric(input$CaptureRate)/100)*as.numeric(input$Quotes)*as.numeric(input$PolicyValue),digits=0)}) #capture gain due to telematics, we are charging 150 to sign in
+  
+    s<-reactive({round((as.numeric(input$Quotes)*as.numeric(input$CaptureRate)/100)*as.numeric(input$CaptureCost),digits=0)}) #cost due to telematic capture
+    
+    w<-reactive({round((14/100)*as.numeric(input$Quotes)*as.numeric(input$PolicyValue),digits=0)})  #average capture gain  
+    
+    x<-reactive({round((14/100)*as.numeric(input$Quotes)*as.numeric(input$CaptureCost),digits=0)}) #cost for average capture
+    v<-reactive({as.numeric(input$Quotes)*as.numeric(input$CaptureCost)}) #cost to quote in income requests
+   
+    
+    X<-reactive({  #disscounts
+        diss<-c(25,18,12,7,0)
+        popu<-c(0.0351,0.228,0.251,0.139,0.3469) #this is a tentative break down to population in each risk group based on Jared's pricing analysis
+        DX<-cbind(diss,popu)
+        DX<-as.data.table(DX)
+        
+        for (i in 1:nrow(DX)){
+            DX$popcount[i]<-(DX$popu[i])*(as.numeric(input$CaptureRate)/100)*as.numeric(input$Quotes)} 
+        for (i in 1:nrow(DX)){
+            DX$TotalDiscount[i]<-DX$popcount[i]*((DX$disscount[i]/100)*input$PolicyValue)}
+        round(sum(DX$TotalDiscount),digits=0)})
+    
+    
+    Y<-reactive({prettyNum(as.numeric(round(t()-v()-X()-w(),digits=0)))})   #the difference between 14% average capture to 18% telematics capture
+    
+    output$CSummary<-renderText({if(input$Telematics>0&input$Quotes>0){paste("    Capture Summary:    $",
+                                                                             prettyNum(Y(),big.mark=","))}})
+    
+    
+    # to caluclate the FINAL ROI value:
+    
+    d<-reactive({abs((((10/100)*(input$TotalDrivers))*2*input$PolicyValue)- #2 is because it costs 2 policies to replace 1 policy
+                         ((((10/100)*(input$TotalDrivers-(input$TotalDrivers*input$Telematics/100)))+
+                               ((as.numeric(input$DefectionRate)/100)*(input$TotalDrivers*input$Telematics/100)))*2*input$PolicyValue))}) #defection rate
+    
+    
+    e<-reactive({input$Technology*40})  #technology
+    f<-reactive({  #disscounts
+        diss<-c(20,15,10,5,0)
+        popu<-c(0.0351,0.228,0.251,0.139,0.3469) #this is a tentative break down to population in each risk group based on Jared's pricing analysis
+        DF<-cbind(diss,popu)
+        DF<-as.data.table(DF)
+        
+        for (i in 1:nrow(DF)){
+            DF$popcount[i]<-(DF$popu[i])*((input$Telematics/100)*input$TotalDrivers)}  
+        for (i in 1:nrow(DF)){
+            DF$TotalDiscount[i]<-DF$popcount[i]*((DF$diss[i]/100)*input$PolicyValue)}
+        round(sum(DF$TotalDiscount),digits=1)})
+    h<-reactive({input$TotalDrivers*input$PolicyValue})
+    
+    y<-reactive({t()-v()-X()-w()})   #capture rate
+    
+    
+    output$GAINS<-renderText({paste("   Gain= $",prettyNum(round(as.numeric(y()+d()-e()-f()),digits=0),big.mark=","))})  #includes capture rates
+    output$ROI<-renderText({paste("   ROI=",prettyNum(round(as.numeric((y()+d()-e()-f())/(e()+f())),digits=3),big.mark=","))})
+    output$Gain_percent<- renderText({paste("[",round(as.numeric((y()+d()-e()-f())/(h()+y())),digits=3)*100,"%] added value")})
+    
+    
+})
+
---
+++ ui.R
@@ -0,0 +1,115 @@
+#this is the UIscript: controls the layout and appearance of your app. 
+#It is defined in a source script named ui.R. 
+### To get around Verisk's fire wall
+Sys.getenv("http_proxy")
+Sys.setenv(http_proxy="http://i57757:Verisk17@proxy.iso.com:8080")
+setInternet2(use = TRUE)
+
+
+
+library(shiny)
+library(shinythemes)
+
+
+fluidPage(
+    
+    h1(tags$b("Return On Investment - Telematics App"),align="center",style = "color:black;font-family:'Ariel';font-size: 36px; line-height: 32px;"),
+    
+    sidebarLayout(
+        sidebarPanel(   #all the side panel options
+            (tags$head(tags$style("body {background-color: silver; }",type='text/css', ".irs-grid-text { font-size: 14pt; }",".form-control{font-size: 18pt; }",
+                                  ".irs-min{font-size: 14pt; }",".irs-max{font-size: 14pt; }",".irs-single {font-size: 14pt; }",
+                                  ".irs-slider single{font-size: 4pt; }",".radio{font-size: 20pt; }"))),  #background color and changing size of bottons
+            #h2("Inputs",align="center",style = "color:blue"),
+            br(),br(),
+            sliderInput("TotalDrivers", tags$p("Portfolio Size",style = "color:black;font-family:'Ariel';font-size: 30px; line-height: 32px;"), min = 0, max = 10000,pre = " drivers ",value = 1000),
+            sliderInput("Telematics", tags$p("Telematics",style = "color:black;font-family:'Ariel';font-size: 30px; line-height: 32px;"), min = 0, max = 100,value = 10, pre = " % "),
+            sliderInput("PolicyValue", tags$p("Policy Value",style = "color:black;font-family:'Ariel';font-size: 30px; line-height: 32px;"), min = 0, max = 2000,value = 1000, pre = " $ "),
+            br(),br(),
+            numericInput("Technology",tags$p("Technology",style = "color:black;font-family:'Ariel';font-size: 30px; line-height: 32px;"),50,min=0,max=10000,step=10),
+            radioButtons("DefectionRate",tags$p("Policy Defection Rate [%]",style = "color:black;font-family:'Ariel';font-size: 30px; line-height: 32px;"),choices = c("2", "3", "4"),selected = "4"),
+            br(),br(),
+            sliderInput("Quotes", tags$p("Number of Quotes",style = "color:black;font-family:'Ariel';font-size: 30px; line-height: 32px;"), min = 0, max = 1000,pre = " Quotes ",value = 100),
+            radioButtons("CaptureRate", tags$p("Capture Rate [%]",style = "color:black;font-family:'Ariel';font-size: 30px; line-height: 32px;"),choices = c("14","16","18"),selected = "16"),
+            radioButtons("CaptureCost", tags$p("Capture Cost [$]",style = "color:black;font-family:'Ariel';font-size: 30px; line-height: 32px;"),choices = c("10","20","30"),selected = "10"),
+            
+            width=4),
+        
+        
+        mainPanel(
+            (tags$head(tags$style("body {background-color: gainsboro; }"))),
+            br(),
+            fluidRow(column(10,offset=8,
+                            textOutput("ROI"),style = "color:darkgreen;font-family:'Ariel';font-size: 48px; line-height: 40px;")),
+            br(),
+            fluidRow(column(10,offset=8,
+                            textOutput("GAINS"),style = "color:darkgreen;font-family:'Ariel';font-size: 48px; line-height: 40px;")),
+            
+            br(),
+            fluidRow(column(10,offset=8,
+                            textOutput("Gain_percent"),style = "color:darkgreen;font-family:'Ariel';font-size: 48px; line-height: 40px;")),
+            
+            
+            fluidRow(column(10,offset=0,
+                            textOutput("TotalPremium"),style = "color:black;font-family:'Ariel';font-size: 30px; line-height: 32px;")),
+            br(),
+            
+            plotOutput("population",width="100%",height="250px"),
+            br(),
+            fluidRow(column(10,offset=0,
+                            textOutput("TotalDiscount"),style = "color:black;font-family:'Ariel';font-size: 30px; line-height: 32px;")),
+            br(),
+            fluidRow(column(10,offset=0,
+                            textOutput("InitialCost"),style = "color:black;font-family:'Ariel';font-size: 30px; line-height: 32px;")),
+            br(),
+            fluidRow(column(10,offset=0,
+                            textOutput("TechnologyCost"),style = "color:black;font-family:'Ariel';font-size: 30px; line-height: 32px;")),
+            
+            br(),br(),br(),
+            
+            
+            fluidRow(column(10,offset=0,
+                            textOutput("DefectionRate"),style = "color:black;font-family:'Ariel';font-size: 30px; line-height: 32px;")),
+            br(),
+           
+            fluidRow(column(10,offset=0,
+                            textOutput("DefectionSavings"),style = "color:black;font-family:'Ariel';font-size: 30px; line-height: 32px;")),
+            br(),br(),br(),br(),
+            
+            
+            ###Capture calculations: 
+            br(),br(),br(),br(),br(),br(),br(),
+            fluidRow(column(10,offset=0,
+                            textOutput("CaptureTotal"),style = "color:black;font-family:'Ariel';font-size: 30px; line-height: 32px;")), 
+            br(),
+            fluidRow(column(10,offset=0,
+                            textOutput("CostTotal"),style = "color:black;font-family:'Ariel';font-size: 30px; line-height: 32px;")), 
+            br(),
+            fluidRow(column(10,offset=0,
+                            textOutput("InitialCost_capture"),style = "color:black;font-family:'Ariel';font-size: 30px; line-height: 32px;")), 
+            br(),
+            fluidRow(column(10,offset=0,
+                            textOutput("S_M"),style = "color:black;font-family:'Ariel';font-size: 30px; line-height: 32px;")), 
+            
+            fluidRow(column(10,offset=0,
+                            textOutput("M_STotal"),style = "color:black;font-family:'Ariel';font-size: 30px; line-height: 32px;")), 
+            fluidRow(column(10,offset=0,
+                            textOutput("CapturePopulation"),style = "color:black;font-family:'Ariel';font-size: 30px; line-height: 32px;")), 
+            fluidRow(column(10,offset=0,
+                            textOutput("CTotal"),style = "color:black;font-family:'Ariel';font-size: 30px; line-height: 32px;")), 
+            fluidRow(column(10,offset=0,
+                            textOutput("CAverage"),style = "color:black;font-family:'Ariel';font-size: 30px; line-height: 32px;")), 
+            br(),
+            fluidRow(column(10,offset=0,
+                            textOutput("CSummary"),style = "color:black;font-family:'Ariel';font-size: 30px; line-height: 32px;")), 
+            
+            br()
+            ,width=8,style = "border: 1.8px solid silver;")
+    )
+)
+
+
+
+
+
+
